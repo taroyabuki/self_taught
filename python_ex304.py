f24ecb6a774604004a77fec23c51816e9959b1a3
Taro Yabuki <taroyabuki@users.noreply.github.com>
2024-07-01 21:19:31 +0900

    Update python_ex304.py
    The same as in the book.

--- python_ex304.py
+++ python_ex304.py
@@ -1,10 +1,7 @@
-# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
-# If you are enjoying it, please consider leaving a review on Amazon :). https://www.amazon.com/dp/B01M01YDQA#customerReviews. Keep up the hard work!
-
+# http://tinyurl.com/jnw56zx
 
 import time
 import random
-
 
 class Queue:
     def __init__(self):
@@ -22,32 +19,24 @@ class Queue:
     def size(self):
         return len(self.items)
 
-    def simulate_line(self,
-                      till_show,
-                      max_time):
-        pq = Queue()
-        tix_sold = []
+def simulate_line(till_show, max_time):
+    pq = Queue()
+    tix_sold = []
 
-        for i in range(10):
-            pq.enqueue("person"
-                       + str(i))
-
-        t_end = time.time()\
-                + till_show
+    for i in range(100):
+        pq.enqueue("person" + str(i))
+    
+    t_end = time.time() + till_show
+    now = time.time()
+    while now < t_end and not pq.is_empty():
         now = time.time()
-        while now < t_end \
-        and not pq.is_empty():
-            now = time.time()
-            r = random.\
-                randint(0,
-                        max_time)
-            time.sleep(r)
-            person = pq.dequeue()
-            print(person)
-            tix_sold.append(person)
+        r = random.randint(0, max_time)
+        time.sleep(r)
+        person = pq.dequeue()
+        print(person)
+        tix_sold.append(person)
+        
+    return tix_sold
 
-        return tix_sold
-
-queue = Queue()
-sold = queue.simulate_line(5, 1)
+sold = simulate_line(5, 1)
 print(sold)
